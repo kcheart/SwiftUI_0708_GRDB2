bbeec9808ad327223e5880faf4806ab8d270fcfc
Kevin Chen <kcheart@gmail.com>
2020-07-08 19:03:56 +0800

    Initial Commit

---
+++ SwiftUI_0708_GRDB2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_0708_GRDB2
+//
+//  Created by Kevin Chen on 2020/7/8.
+//  Copyright © 2020 Kevin Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
